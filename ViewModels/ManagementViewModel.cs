bf9cb0e35452a70169df161135a7a203a7648486
admin <admin@DESKTOP-FA5KDQG>
2024-06-18 19:33:40 +0300

    Add project files.

---
+++ ViewModels/ManagementViewModel.cs
@@ -0,0 +1,135 @@
+﻿using Project_Manager.Commands;
+using Project_Manager.Models;
+using Project_Manager.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Project_Manager.ViewModels
+{
+    public class ManagementViewModel : ViewModelBase
+    {
+        private ObservableCollection<Project> _projects;
+        private Project selectedProject;
+        private Models.Task selectedTask;
+
+        public ObservableCollection<Project> Projects
+        {
+            get => _projects;
+            set
+            {
+                _projects = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Project SelectedProject
+        {
+            get => selectedProject;
+            set
+            {
+                selectedProject = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Models.Task SelectedTask
+        {
+            get => selectedTask;
+            set
+            {
+                selectedTask = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddProjectCommand { get; }
+        public ICommand EditProjectCommand { get; }
+        public ICommand DeleteProjectCommand { get; }
+        public ICommand AddTaskCommand { get; }
+        public ICommand EditTaskCommand { get; }
+        public ICommand DeleteTaskCommand { get; }
+
+        public ManagementViewModel()
+        {
+            Projects = new ObservableCollection<Project>();
+            AddProjectCommand = new RelayCommand(AddProject);
+            EditProjectCommand = new RelayCommand(EditProject, CanEditOrDeleteProject);
+            DeleteProjectCommand = new RelayCommand(DeleteProject, CanEditOrDeleteProject);
+            AddTaskCommand = new RelayCommand(AddTask);
+            EditTaskCommand = new RelayCommand(EditTask, CanEditOrDeleteTask);
+            DeleteTaskCommand = new RelayCommand(DeleteTask, CanEditOrDeleteTask);
+        }
+
+        private void AddProject()
+        {
+            AddProjectForm addProjectForm = new AddProjectForm();
+            addProjectForm.AddProject += AddProjectForm_AddProject;
+            addProjectForm.ShowDialog();
+        }
+
+        private void AddProjectForm_AddProject(Project obj)
+        {
+            Projects.Add(obj);
+        }
+
+        private void EditProject()
+        {
+            AddProjectForm addProjectForm = new AddProjectForm(SelectedProject);
+            addProjectForm.ShowDialog();
+        }
+
+        private void DeleteProject()
+        {
+            if (SelectedProject != null)
+            {
+                Projects.Remove(SelectedProject);
+                SelectedProject = null;
+            }
+        }
+
+        private bool CanEditOrDeleteProject()
+        {
+            return SelectedProject != null;
+        }
+
+        private void AddTask()
+        {
+            if (SelectedProject != null)
+            {
+                AddTaskForm addTaskForm = new AddTaskForm();
+                addTaskForm.AddTask += AddTaskForm_AddTask;
+                addTaskForm.ShowDialog();
+            }
+        }
+
+        private void AddTaskForm_AddTask(Models.Task obj)
+        {
+            SelectedProject.Tasks.Add(obj);
+        }
+
+        private void EditTask()
+        {
+            AddTaskForm addTaskForm = new AddTaskForm(SelectedTask);
+            addTaskForm.ShowDialog();
+        }
+
+        private void DeleteTask()
+        {
+            if (SelectedProject != null && SelectedTask != null)
+            {
+                SelectedProject.Tasks.Remove(SelectedTask);
+                SelectedTask = null;
+            }
+        }
+
+        private bool CanEditOrDeleteTask()
+        {
+            return SelectedProject != null && SelectedTask != null;
+        }
+    }
+}
---
+++ Views/AddTaskForm.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Project_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_Manager.Views
+{
+    /// <summary>
+    /// Interaction logic for AddTaskForm.xaml
+    /// </summary>
+    public partial class AddTaskForm : Window
+    {
+        private Models.Task task;
+        public event Action<Models.Task> AddTask;
+
+        public AddTaskForm()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+        }
+        public AddTaskForm(Models.Task task)
+        {
+            InitializeComponent();
+            this.task = task;
+            DataContext = task;
+        }
+        public Priority SelectedPriority { get; set; }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (task == null)
+            {
+                Models.Task task = new Models.Task()
+                {
+                    Name = nameTextBox.Text,
+                    Description = descriptionTextBox.Text,
+                    Priority = SelectedPriority,
+                    IsCompleted = (bool)statusCheckBox.IsChecked
+                };
+
+                AddTask?.Invoke(task);
+            }
+            else
+            {
+                task.Name = nameTextBox.Text;
+                task.Description = descriptionTextBox.Text;
+                task.Priority = SelectedPriority;
+                task.IsCompleted = (bool)statusCheckBox.IsChecked;
+            }
+            Close();
+        }
+    }
+}
